d64890e5c30e7598447f6503f07d0099ad459398
Adam <adam@summers.com.tw>
2015-01-07 11:04:13 +0800

    add search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,9 +16,11 @@ class SearchController < ApplicationController
 
     @q = Hqproduct.with_translations(I18n.locale).where(showatfront: 200).search(params[:q].try(:merge, m: 'or'))
     @hq_result = @q.result(distinct: true)
+    @hq_has_results = !@hq_result.nil? && @hq_result.count > 0 ? true : false
 
     @q = Brproduct.with_translations(I18n.locale).where(showatfront: 200).search(params[:q].try(:merge, m: 'or'))
     @br_result = @q.result(distinct: true)
+    @br_has_results = !@br_result.nil? && @br_result.count > 0 ? true : false
 
 
     #unless params[:search].blank?
